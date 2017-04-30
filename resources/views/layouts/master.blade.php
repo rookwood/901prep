88abc9a32882dc4a2b8301e9da50a335aac1cdd0
Matt Hollis <matthollis@gmail.com>
2017-04-30 09:15:47 -0500

    Concat css and js files, optimize images

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,11 +11,7 @@
 
     <title>901Prep - ACT prep and tutoring for Memphis</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/agency.css" rel="stylesheet">
+    <link href="css/901prep.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
     <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -39,24 +35,11 @@
     @yield('content')
 
     @include('partials.footer')
-    
-    <!-- jQuery Version 1.11.0 -->
-    <script src="js/jquery-1.11.0.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/901prep.js"></script>
 
     <!-- Plugin JavaScript -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="js/classie.js"></script>
-    <script src="js/cbpAnimatedHeader.js"></script>
-
-    <!-- Contact Form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="js/agency.js"></script>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,17 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.scripts([
+    'resources/assets/js/agency.js',
+    'resources/assets/js/app.js',
+    'resources/assets/js/boostrap.js',
+    'resources/assets/js/cbpAnimatedHeader.js',
+    'resources/assets/js/classie.js',
+    'resources/assets/js/contact_me.js',
+    'resources/assets/js/jqBootstrapValidation.js',
+    'resources/assets/js/jquery-1.11.0.js',
+], 'public/js/901prep.js').styles([
+    'resources/assets/css/agency.css',
+    'resources/assets/css/app.css',
+    'resources/assets/css/bootstrap.css',
+], 'public/css/901prep.css');

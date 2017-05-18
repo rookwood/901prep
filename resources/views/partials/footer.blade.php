1c969bd80b2d573425481a719403a2c2d398f9b3
Matt Hollis <matthollis@gmail.com>
2017-05-18 16:55:10 -0500

    update photo credit

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,7 +8,7 @@
                     <span class="copyright">Proudly hosted on <a href="https://www.digitalocean.com/?refcode=b1633caa9cde">Digital Ocean</a></span>
                 </div>
                 <div class="col-md-4">
-                    <span class="copyright">Bridge photo courtesy of <a href="http://www.reddit.com/user/luigivampa-over9000">/u/Luigivampa</a></span>
+                    <span class="copyright">901 Prep logo by <a href="http://emkatdesign.com">Emily Harvey</a></span>
                 </div>
                 <div class="col-md-3">
                     <span class="copyright">Built by <a href="https://twitter.com/somehollis">Matt Hollis</a></span>

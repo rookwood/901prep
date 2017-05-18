4fbe055dd13c06712aaf6c6e98aac08a0cef9db6
Matt Hollis <matthollis@gmail.com>
2017-05-17 20:54:52 -0500

    New logo

--- resources/views/partials/home/header.blade.php
+++ resources/views/partials/home/header.blade.php
@@ -2,9 +2,9 @@
     <header>
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in">Superior ACT preparation and tutoring</div>
-                <div class="intro-heading">901 Prep</div>
-                <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+                <div class="intro-lead-in"></div>
+                <div class="intro-heading"></div>
+                <a href="#services" class="btn-intro page-scroll btn btn-xl">Tell Me More</a>
             </div>
         </div>
     </header>

d21301cd559c54335f2365fd363f9b54731dee0c
Matt Hollis <matthollis@gmail.com>
2019-10-10 10:16:53 -0500

    Different spam approach; filter out submissions that contain urls

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,6 +24,16 @@ class ContactController extends Controller
     {
         list($name, $phone, $email, $message) = array_values($request->only(['name', 'phone', 'email', 'message']));
 
+        if (str_contains($message, 'http')) {
+            // Likely spam
+            Log::info('------------Likely spam message------------');
+            Log::info("Mail request from {$name} ({$phone}})");
+            Log::info("Did not send email from {$email} with attached message: {$message}");
+            Log::info("-----------------------------------------------------------");
+
+            return Response::json('Sorry, this looks like spam.  We don\'t allow URLs in contact info', 422);
+        }
+
         try {
             $this->mail->to(config('mail.contactEmail'))
                 ->send(new CustomerContact($name, $phone, $email, $message));
